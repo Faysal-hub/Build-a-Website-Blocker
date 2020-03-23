b2f11f1a2afb13d4f3f4500b0d5d57ef5c610fb7
Faysal-hub <ovitahsan@gmail.com>
2020-03-23 22:03:56 +0100

    chanage the time duration

--- website_blocker.py
+++ website_blocker.py
@@ -7,7 +7,7 @@ redirect="127.0.0.1"
 website_list=["www.facebook.com","facebook.com","dub119.mail.live.com","www.dub119.mail.live.com"]
 
 while True:
-    if dt(dt.now().year,dt.now().month,dt.now().day,10) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,16):
+    if dt(dt.now().year,dt.now().month,dt.now().day,10) < dt.now() < dt(dt.now().year,dt.now().month,dt.now().day,17):
         print("Working hours...")
         with open(hosts_path,'r+') as file:
             content=file.read()
